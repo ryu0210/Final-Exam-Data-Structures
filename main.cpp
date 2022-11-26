d1c3467aa28dd631d34e37df8bab4c21856a3e2f
Ashleyhendrawan <92733182+Ashleyhendrawan@users.noreply.github.com>
2022-11-26 17:56:30 +0700

    I declare and create data
    I create Stack Data Structure, and declared various variables

--- main.cpp
+++ main.cpp
@@ -7,6 +7,35 @@
 
 using namespace std;
 
+#define max 10
+#define SIZE 5
+string marks[max];
+
+int top = 0;
+int items[SIZE], front, rear;
+
+/** ================================================================================================ */
+
+/// For Number 2: Creating Stack Data Structure
+
+//to see if stack is full or not
+bool isFull(){
+	if(top >= max){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// push function (insert)
+void push(float score){
+	if(!isFull()){
+
+		marks[top] = score;
+		top++;
+	}
+}
+
 int main(){
   bool limit = true;
     CourseAdministration Sampoerna;
