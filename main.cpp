f80561c3ac7c5553695cac5f3ae9da4cfea63d2e
aurelienathania <115788374+aurelienathania@users.noreply.github.com>
2022-11-28 14:51:38 +0700

    I added Course Registration
    In this part, the student will choose the courses they want to apply, under the maximum credits set by the program

--- main.cpp
+++ main.cpp
@@ -228,3 +228,284 @@ int main(){
 		cout << Name << " failed the semester!" << endl;
 		FinalCredits = 0;
 	}
+/** =========================================================================== */
+
+    /// To Insert Courses Student has taken
+
+    cout << "==============================================================================================" << endl;
+
+    cout << "\nMaximum Credits for Student to take: " << FinalCredits << endl;
+
+    int LimitedCredits = 0; /// To add more credits using operator assignment, initialized with zero
+    int choice2; /// Button to choose credit
+    int z = 0; /// Size of the number of courses
+    string CourseNo[100] = {}; /// Courses are defined using string arrays
+
+    int arr2[8] = {}; //arr so the user dont pick the same option twice
+
+    if(FinalCredits == 24){
+        while(!(LimitedCredits > 21)){
+            Sampoerna.CurrentSem();
+            cin >> choice2;
+
+            if(arr2[0] == choice2 || arr2[1] == choice2 || arr2[2] == choice2 || arr2[3] == choice2 || arr2[4] == choice2 || arr2[5] == choice2 || arr2[6] == choice2 || arr2[7] == choice2){
+                cout << "You have chosen this lesson\n" << "Choose a different lesson\n" << endl;
+                continue;
+            }
+
+
+            else if( choice2 == 1){
+                CourseNo[z] = "Object Oriented Programming(5cr)";
+                z++;
+                arr2[0] = 1;
+                LimitedCredits += 5;
+                cout << "Object Oriented Programming has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 2){
+                CourseNo[z] = "Kewarganegaraan(2cr)";
+                z++;
+                arr2[1] = 2;
+                LimitedCredits += 2;
+                cout <<  "Kewarganegaraan has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 3){
+                CourseNo[z] = "Biology(3cr) ";
+                z++;
+                arr2[2] = 3;
+                LimitedCredits += 3;
+                cout << "Biology has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 4){
+                CourseNo[z] = "Macroeconomics(3cr)";
+                z++;
+                arr2[3] = 4;
+                LimitedCredits += 3;
+                cout << "Macroeconomics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 5){
+                CourseNo[z] = "Chemistry(4cr)";
+                z++;
+                arr2[4] = 5;
+                LimitedCredits += 4;
+                cout << "Chemistry has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 6){
+                CourseNo[z] = "Principles of IS(3cr)";
+                z++;
+                arr2[5] = 6;
+                LimitedCredits += 3;
+                cout << "Principles of IS has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 7){
+                CourseNo[z] = "Mobile Development(3cr)";
+                z++;
+                arr2[6] = 7;
+                LimitedCredits += 3;
+                cout << "Mobile Development has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 8){
+                CourseNo[z] = "Statistics(4cr)";
+                z++;
+                arr2[7] = 8;
+                LimitedCredits += 4;
+                cout << "Statistics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+
+    else if(FinalCredits == 21){
+        while(!(LimitedCredits > 18)){
+            cout << "\nLessons: " << "\n1. Object Oriented Programming(5cr) " << "\n2. Kewarganegaraan(2cr) " << "\n3. Biology(3cr) " << "\n4. Macroeconomics(3cr) " << "\n5. Chemistry(4cr) " << "\n6. Principles of IS(3cr)" << "\n7. Mobile Development (3cr)" << "\n8. Statistics (4cr)" << "\nPick Courses [1-8] to Apply:";
+            cin >> choice2;
+
+            if(arr2[0] == choice2 || arr2[1] == choice2 || arr2[2] == choice2 || arr2[3] == choice2 || arr2[4] == choice2 || arr2[5] == choice2|| arr2[6] == choice2 || arr2[7] == choice2){
+                cout << "You have chosen this lesson\n" << "Choose a different lesson\n" << endl;
+                continue;
+            }
+
+            else if( choice2 == 1){
+                CourseNo[z] = "Object Oriented Programming(5cr)";
+                z++;
+                arr2[0] = 1;
+                LimitedCredits += 5;
+                cout << "Object Oriented Programming has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 2){
+                CourseNo[z] = "Kewarganegaraan(2cr)";
+                z++;
+                arr2[1] = 2;
+                LimitedCredits += 2;
+                cout <<  "Kewarganegaraan has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 3){
+                CourseNo[z] = "Biology(3cr) ";
+                z++;
+                arr2[2] = 3;
+                LimitedCredits += 3;
+                cout << "Biology has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 4){
+                CourseNo[z] = "Macroeconomics(3cr)";
+                z++;
+                arr2[3] = 4;
+                LimitedCredits += 3;
+                cout << "Macroeconomics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 5){
+                CourseNo[z] = "Chemistry(4cr)";
+                z++;
+                arr2[4] = 5;
+                LimitedCredits += 4;
+                cout << "Chemistry has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 6){
+                CourseNo[z] = "Principles of IS(3cr)";
+                z++;
+                arr2[5] = 6;
+                LimitedCredits += 3;
+                cout << "Principles of IS has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 7){
+                CourseNo[z] = "Mobile Development(3cr)";
+                z++;
+                arr2[6] = 7;
+                LimitedCredits += 3;
+                cout << "Mobile Development has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 8){
+                CourseNo[z] = "Statistics(4cr)";
+                z++;
+                arr2[7] = 8;
+                LimitedCredits += 4;
+                cout << "Statistics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+
+    else if(FinalCredits == 18){
+        while(!(LimitedCredits > 15)){
+            cout << "\nLessons: " << "\n1. Object Oriented Programming(5cr) " << "\n2. Kewarganegaraan(2cr) " << "\n3. Biology(3cr) " << "\n4. Macroeconomics(3cr) " << "\n5. Chemistry(4cr) " << "\n6. Principles of IS(3cr)" << "\n7. Mobile Development (3cr)" << "\n8. Statistics (4cr)" << "\nPick Courses [1-8] to Apply:";
+            cin >> choice2;
+
+            if(arr2[0] == choice2 || arr2[1] == choice2 || arr2[2] == choice2 || arr2[3] == choice2 || arr2[4] == choice2 || arr2[5] == choice2|| arr2[6] == choice2 || arr2[7] == choice2){
+                cout << "You have chosen this lesson\n" << "Choose a different lesson\n" << endl;
+                continue;
+            }
+            else if( choice2 == 1){
+                CourseNo[z] = "Object Oriented Programming(5cr)";
+                z++;
+                arr2[0] = 1;
+                LimitedCredits += 5;
+                cout << "Object Oriented Programming has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 2){
+                CourseNo[z] = "Kewarganegaraan(2cr)";
+                z++;
+                arr2[1] = 2;
+                LimitedCredits += 2;
+                cout << "Kewarganegaraan has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 3){
+                CourseNo[z] = "Biology(3cr)";
+                z++;
+                arr2[2] = 3;
+                LimitedCredits += 3;
+                cout << "Biology has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 4){
+                CourseNo[z] = "Macroeconomics(3cr)";
+                z++;
+                arr2[3] = 4;
+                LimitedCredits += 3;
+                cout << "Macroeconomics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 5){
+                CourseNo[z] = "Chemistry(4cr)";
+                z++;
+                arr2[4] = 5;
+                LimitedCredits += 4;
+                cout << "Chemistry has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 6){
+                CourseNo[z] = "Principles of IS(3cr)";
+                z++;
+                arr2[5] = 6;
+                LimitedCredits += 3;
+                cout << "Principles of IS has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 7){
+                CourseNo[z] = "Mobile Development(3cr)";
+                z++;
+                arr2[6] = 7;
+                LimitedCredits += 3;
+                cout << "Mobile Development has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+
+            else if( choice2 == 8){
+                CourseNo[z] = "Statistics(4cr)";
+                z++;
+                arr2[7] = 8;
+                LimitedCredits += 4;
+                cout << "Statistics has been chosen!" << endl;
+                cout << "\nTotal Credits: " << LimitedCredits << endl;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+
+
+    cout << "\nApplication Completed, please re-evaluate!\n" << endl;
